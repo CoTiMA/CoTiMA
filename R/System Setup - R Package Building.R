e1391d5e1a0eb682044e71d2a616b6249fb8cbfb
Markus Homberg <markus.homberg@uni-mainz.de>
2020-02-20 13:13:57 +0100

    CoTiMA PacDev

--- R/System Setup - R Package Building.R
+++ R/System Setup - R Package Building.R
@@ -8,7 +8,9 @@ install.packages(c("devtools",
                    "knitr",
                    "usethis",
                    "rmarkdown",
-                   "pacman"))
+                   "pacman",
+                   "tidyverse"
+                   "fs"))
 
 library(pacman)
 p_load(devtools,
@@ -16,11 +18,24 @@ p_load(devtools,
        roxygen2,
        testthat,
        usethis,
-       rmarkdown)
+       rmarkdown,
+       tidyverse,
+       fs)
 
 has_devel()
 
 
 usethis::use_build_ignore("System Setup - R Package Building.R")
+usethis::use_build_ignore("Testfile.R")
 
 load_all()
+check()
+#use_mit_license("Markus Homberg")
+document()
+install()
+use_testthat()
+use_test("functionname")
+test()
+use_package("packagename")
+use_readme_rmd()
+rmarkdown::render("README.Rmd")
