45458ac9dd2cfaa9e59a67545189d1ff816c3aea
christiandormann <cdormann@uni-mainz.de>
2021-02-10 09:10:20 +0100

    first examples checked example data added

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Object created with ctmaPrep
+#' @name CoTiMAstudyList1
+#' @docType data
+#' @author C. Dormann & M. Homburg \email{CoTiMA@@uni-mainz.org}
+#' @keywords data
+"CoTiMAstudyList1"
+
+
+#' Object created with ctmaInit
+#' @name CoTiMAInitFit1
+#' @docType data
+#' @author C. Dormann & M. Homburg \email{CoTiMA@@uni-mainz.org}
+#' @keywords data
+"CoTiMAInitFit1"
+
+
