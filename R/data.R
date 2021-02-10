42ef39236fa297a14a2227a7701bdcaa9ef90ee1
christiandormann <cdormann@uni-mainz.de>
2021-02-10 09:29:14 +0100

    minors

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Object created with ctmaPrep
+#' Object created with ctmaPrep coprising 3 studies
 #' @name CoTiMAstudyList1
 #' @docType data
 #' @author C. Dormann & M. Homburg \email{CoTiMA@@uni-mainz.org}
@@ -6,11 +6,18 @@
 "CoTiMAstudyList1"
 
 
-#' Object created with ctmaInit
+#' Object created with ctmaInit of 3 primary studies
 #' @name CoTiMAInitFit1
 #' @docType data
 #' @author C. Dormann & M. Homburg \email{CoTiMA@@uni-mainz.org}
 #' @keywords data
 "CoTiMAInitFit1"
 
+
+#' Object created with ctmaFit using CoTiMAInitFit1 as argument
+#' @name CoTiMAFullFit1
+#' @docType data
+#' @author C. Dormann & M. Homburg \email{CoTiMA@@uni-mainz.org}
+#' @keywords data
+"CoTiMAFullFit1"
 
