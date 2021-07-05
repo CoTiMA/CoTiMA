5d4523846fc91e6073414ea281b31b9cc9c1d400
christiandormann <cdormann@uni-mainz.de>
2021-07-05 15:11:30 +0200

    xls output - minors (column to row vector)

--- R/ctmaFit.R
+++ R/ctmaFit.R
@@ -973,8 +973,9 @@ ctmaFit <- function(
     startCol <- 2; startCol
     startRow <- 2; startRow
     openxlsx::writeData(wb, sheet2, startCol=startCol, startRow = startRow, colNames = FALSE,
-                        c(t(matrix(unlist(results$modelResults$DRIFT),
-                               nrow=n.latent, ncol=n.latent))))
+                        t(c(t(matrix(unlist(results$modelResults$DRIFT),
+                               nrow=n.latent, ncol=n.latent)))))
+    openXL(wb)
     startCol <- 1; startCol
     startRow <- 2; startRow
     openxlsx::writeData(wb, sheet2, startCol=startCol, startRow = startRow, colNames = FALSE,
