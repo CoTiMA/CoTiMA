d26848d39f0fc1851924a37ee82fc24d236732b3
christiandormann <cdormann@uni-mainz.de>
2021-11-10 07:56:06 +0100

    minor

--- R/ctmaPrep.R
+++ R/ctmaPrep.R
@@ -117,7 +117,6 @@ ctmaPrep <- function(selectedStudies=NULL,
   }
 
   if (is.null(activeDirectory)) {
-    if (activateRPB==TRUE) {RPushbullet::pbPost("note", paste0("CoTiMA (",Sys.time(),")" ), paste0(Sys.info()[[4]], "\n","Data processing stopped.\nYour attention is required."))}
     ErrorMsg <- "\nNo active directory has been specified! \nGood luck for the next try!"
     stop(ErrorMsg)
   }
