7ba3334ca4c538f71b21fde92f657802f811ebd0
christiandormann <cdormann@uni-mainz.de>
2022-09-16 14:08:52 +0200

    minor

--- R/ctmaInit.R
+++ R/ctmaInit.R
@@ -28,7 +28,7 @@
 #' @param iter number of interation (defaul = 1000). Sometimes larger values could be required fom Bayesian estimation
 #' @param verbose integer from 0 to 2. Higher values print more information during model fit - for debugging
 #' @param customPar logical. If set TRUE leverages the first pass using priors and ensure that the drift diagonal cannot easily go too negative (helps since ctsem > 3.4)
-#' @param doPar parallel and multiple fitting if single studies
+#' @param doPar parallel and multiple fitting if single studies. A value > 1 will fit each study doPar times in parallel mode during which no output is generated (screen remains silent). Useful to obtain best fit.
 #' @param useSV if TRUE (default) start values will be used if provided in the list of primary studies
 #' @param experimental set TRUE to try new pairwise N function
 #' @param T0means Default 0 (assuming standardized variables). Can be assigned labels to estimate them freely.
