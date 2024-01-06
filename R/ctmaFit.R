8980143887db52f6e1308209e11dd332f8c7dc98
christiandormann <cdormann@uni-mainz.de>
2024-01-06 16:47:32 +0100

    CoTiMA::CoTiMAStanctArgs used to fill CoTiMAStanctArgs argument if it is NULL

--- R/ctmaFit.R
+++ R/ctmaFit.R
@@ -170,7 +170,7 @@ ctmaFit <- function(
     if (is.null(cint)) cint <- 0
     if (is.null(fit)) fit <- TRUE
     if (is.null(WEC)) WEC <- FALSE
-    if (is.null(CoTiMAStanctArgs)) <- CoTiMAStanctArgs <- CoTiMA::CoTiMAStanctArgs
+    if (is.null(CoTiMAStanctArgs)) CoTiMAStanctArgs <- CoTiMA::CoTiMAStanctArgs
     }
 
 
