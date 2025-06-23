bcd846bc1969cccc3b9341461631793ec29215b0
christiandormann <cdormann@uni-mainz.de>
2025-06-23 13:45:14 +0200

    undo last change

--- R/ctmaInit.R
+++ R/ctmaInit.R
@@ -950,6 +950,8 @@ ctmaInit <- function(
           }
         } # end  if ( (randomIntercepts != TRUE) & (randomIntercepts != "MANIFEST") )
 
+        if (packageDescription("ctsem")$Version > "3.10.2") type <- "ct" else type <- "stanct"
+
         currentModel <- suppressMessages(
           ctsem::ctModel(n.latent=n.latent, n.manifest=n.var, Tpoints=currentTpoints, manifestNames=manifestNames,    # 2 waves in the template only
                          DIFFUSION=matrix(diffParamsTmp, nrow=n.latent, ncol=n.latent), #, byrow=TRUE),
@@ -1020,6 +1022,8 @@ ctmaInit <- function(
           manifestNamesTmp <- manifestNames; manifestNamesTmp
           latentNamesTmp <- c(latentNames, paste0(latentNames, "_cint")); latentNamesTmp
           T0MEANStmp <- matrix(paste0("Mean", latentNamesTmp), n.latent*2, 1); T0MEANStmp
+
+          if (packageDescription("ctsem")$Version > "3.10.2") type <- "ct" else type <- "stanct"
 
           currentModel <- (
             ctsem::ctModel(n.latent=n.latent*2,
